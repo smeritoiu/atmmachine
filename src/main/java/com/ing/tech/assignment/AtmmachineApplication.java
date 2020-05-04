38e2afcbb7259fb2300502df936687324a769fb8
smeritoiu <silvia.galusca92@gmail.com>
2020-05-04 12:27:16 +0300

    initial commit

---
+++ src/main/java/com/ing/tech/assignment/AtmmachineApplication.java
@@ -0,0 +1,13 @@
+package com.ing.tech.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmmachineApplication.class, args);
+	}
+
+}
